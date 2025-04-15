90147e489dee99c0e9af442697d1ea5940ffbc18
soumyata1997@gmail.com <soumyata1997@gmail.com>
2025-04-15 03:19:15 -0400

    Added NASA api service

---
+++ GitHubActions_CICD_Pipeline/Models/EpicImageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubActions_CICD_Pipeline.Models
+{
+    public class EpicImageViewModel
+    {
+        public string? Caption { get; set; }
+        public string? Date { get; set; }
+        public string? ImageName { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
